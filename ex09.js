7b6b59a0c38d41e5c67849457b711c61efaf4898
Anderson Souza Gomes <derson3001@gmail.com>
2025-01-11 17:39:08 -0300

    Nono desafio

---
+++ ex09.js
@@ -0,0 +1,96 @@
+//Nono desafio do processo seletivo da IN junior
+//Preferi deixar separado do oitavo por uma questão de organização
+
+function authors(){
+    let resultado = [];
+    let repetido = false;
+    for (categoria of booksByCategory){
+        for (livro of categoria.books){
+            repetido = resultado.includes(livro.author);
+            if (!repetido){
+                resultado.push(livro.author)
+            }
+        }
+    }
+    return resultado;
+}
+const booksByCategory = [
+    {
+        category: "Romance",
+        books: [
+            {
+                title: "O Sol Também é Uma Estrela",
+                author: "Nicola Yoon",
+            },
+            {
+                title: "O Noivo da Minha Melhor Amiga",
+                author: "Emily Giffin",
+            },
+            {
+                title: "Orgulho e Preconceito",
+                author: "Jane Austen",
+            },
+        ],
+    },
+    {
+        category: "Fantasia",
+        books: [
+            {
+                title: "Harry Potter e a Pedra Filosofal",
+                author: "J.K. Rowling",
+            },
+            {
+                title: "O Hobbit",
+                author: "J.R.R. Tolkien",
+            },
+            {
+                title: "Eragon",
+                author: "Christopher Paolini",
+            },
+            {
+                title: "O Nome do Vento",
+                author: "Patrick Rothfuss",
+            },
+        ],
+    },
+    {
+        category: "Suspense",
+        books: [
+            {
+                title: "A Garota no Trem",
+                author: "Paula Hawkins",
+            },
+            {
+                title: "O Silêncio dos Inocentes",
+                author: "Thomas Harris",
+            },
+        ],
+    },
+    {
+        category: "Ficção Científica",
+        books: [
+            {
+                title: "Duna",
+                author: "Frank Herbert",
+            },
+            {
+                title: "Neuromancer",
+                author: "William Gibson",
+            },
+            {
+                title: "Fundação",
+                author: "Isaac Asimov",
+            },
+            {
+                title: "1984",
+                author: "George Orwell",
+            },
+            {
+                title: "O Fim da Infância",
+                author: "Arthur C. Clarke",
+            },
+        ],
+    },
+];
+
+console.log(authors());
